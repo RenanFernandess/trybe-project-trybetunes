48b0feadc5073cea7fd9a4137bd1b81e8abdd863
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 18:47:51 -0300

    REQUISITO-3: Cria um componente de cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import Loading from './Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      userName: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const { name } = await getUser();
+      this.setState({ userName: name, loading: false });
+    });
+  }
+
   render() {
+    const { loading, userName } = this.state;
     return (
       <header data-testid="header-component">
-        <strong data-testid="header-user-name">o</strong>
+        { loading
+          ? <Loading />
+          : (
+            <strong
+              data-testid="header-user-name"
+            >
+              { userName }
+            </strong>) }
       </header>
     );
   }
