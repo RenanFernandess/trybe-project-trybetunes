f10e40b1db87fe05f0e25e71052514a76dd17924
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 18:23:58 -0300

    Renderiza o componente Header na pagina ProfileEdit

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class ProfileEdit extends Component {
   render() {
     return (
       <section data-testid="page-profile-edit">
+        <Header />
         ProfileEdit
       </section>
     );
