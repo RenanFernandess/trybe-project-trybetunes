3d445bcae6f6927fedd055f69305c5561616590e
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 15:47:00 -0300

    Cria o componente ProfileEdit

---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <section data-testid="page-profile-edit">
+        ProfileEdit
+      </section>
+    );
+  }
+}
+
+export default ProfileEdit;
