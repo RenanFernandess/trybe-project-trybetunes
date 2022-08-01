d64390f5412476cf713e4326b440c1ffaa61b11e
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 15:43:15 -0300

    Cria o componente Login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <section data-testid="page-login">
+        Login
+      </section>
+    );
+  }
+}
+
+export default Login;
