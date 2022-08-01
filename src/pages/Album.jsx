08ea901a2563f82cdc3e805543a0c943c3197a1d
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 15:44:56 -0300

    Cria o componente Album

---
+++ src/pages/Album.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <section data-testid="page-album">
+        Album
+      </section>
+    );
+  }
+}
+
+export default Album;
