9dbe89e3e17e122b6c810e278443103ce78b382f
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 18:10:36 -0300

    REQUISITO-2: Cria um formulário para identificação

--- src/App.js
+++ src/App.js
@@ -7,14 +7,56 @@ import Profile from './pages/Profile';
 import Search from './pages/Search';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
+import Loading from './components/Loading';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onClickButtonLogin = this.onClickButtonLogin.bind(this);
+
+    this.state = {
+      loading: false,
+      loginName: '',
+      buttonLoginDisabled: true,
+    };
+  }
+
+  onInputChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => {
+      const { loginName } = this.state;
+      const minCharacter = 3;
+      this.setState({ buttonLoginDisabled: loginName.length < minCharacter });
+    });
+  }
+
+  onClickButtonLogin() {
+    this.setState({ loading: true }, async () => {
+      const { loginName } = this.state;
+      await createUser({ name: loginName });
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
+    const { loginName, buttonLoginDisabled, loading } = this.state;
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/search" component={ Search } />
+          <Route
+            exact
+            path="/"
+            render={ (props) => (<Login
+              { ...props }
+              loginName={ loginName }
+              buttonLoginDisabled={ buttonLoginDisabled }
+              onInputChange={ this.onInputChange }
+              onClickButtonLogin={ this.onClickButtonLogin }
+            />) }
+          />
+          { loading ? <Loading /> : <Route exact path="/search" component={ Search } /> }
           <Route exact path="/album/:id" component={ Album } />
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   render() {
+    const {
+      loginName,
+      buttonLoginDisabled,
+      onInputChange,
+      onClickButtonLogin,
+    } = this.props;
     return (
       <section data-testid="page-login">
-        Login
+        <div>
+          <strong>Nome de login</strong>
+          <input
+            type="text"
+            value={ loginName }
+            name="loginName"
+            onInput={ onInputChange }
+            data-testid="login-name-input"
+          />
+        </div>
+        <Link to="/search">
+          <button
+            type="button"
+            disabled={ buttonLoginDisabled }
+            data-testid="login-submit-button"
+            onClick={ onClickButtonLogin }
+          >
+            Entrar
+          </button>
+        </Link>
       </section>
     );
   }
 }
+
+Login.propTypes = {
+  loginName: propTypes.string.isRequired,
+  buttonLoginDisabled: propTypes.bool.isRequired,
+  onInputChange: propTypes.func.isRequired,
+  onClickButtonLogin: propTypes.func.isRequired,
+};
 
 export default Login;
