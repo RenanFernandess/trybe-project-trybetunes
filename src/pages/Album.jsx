f032a83d58a25273ed870e63f4ceed59a6af9434
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 18:22:18 -0300

    Renderiza o componente Header na pagina Album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Album extends Component {
   render() {
     return (
       <section data-testid="page-album">
+        <Header />
         Album
       </section>
     );
