da0fdf9a17061cb54d75735a2785a3c5a267d5a9
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 18:17:43 -0300

    Cria o componente Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header data-testid="header-component">
+        <strong data-testid="header-user-name">o</strong>
+      </header>
+    );
+  }
+}
+
+export default Header;
