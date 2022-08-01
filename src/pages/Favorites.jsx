3e99f8dd84414ebb5322c7ac081ee4afb963a4b7
Renan Fernandes <renzinestuods@gmail.com>
2022-08-01 18:21:06 -0300

    Renderiza o componente Header na pagina Favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Favorites extends Component {
   render() {
     return (
       <section data-testid="page-favorites">
+        <Header />
         Favorites
       </section>
     );
