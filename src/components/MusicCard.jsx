3123f3abde21425add60ff8f6cc3e2aadea2a92c
Renan Fernandes <renzinestuods@gmail.com>
2022-08-02 20:02:51 -0300

    REQUISITO-8: Cria o mecanismo para adicionar músicas na lista de músicas favoritas

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -2,19 +2,51 @@ import React, { Component } from 'react';
 import propTypes from 'prop-types';
 
 class MusicCard extends Component {
+  // onInputChange = ({ target: { type, name, value, checked } }) => {
+  //   this.setState({ [name]: (type === 'checkbox') ? checked : value });
+  // }
+
+  // componentDidMount() {
+  //   this.setState({}, async () => {
+  //     const { trackId } = this.props;
+  //     const favorited = favoritesList.some(({ trackId: id }) => trackId === id);
+
+  //     this.setState({ favorited });
+  //   });
+  // }
+
   render() {
-    const { trackName, trackViewUrl } = this.props;
+    const {
+      trackName,
+      previewUrl,
+      trackId,
+      onFavoriteChange,
+      favorited,
+    } = this.props;
 
     return (
       <section>
         <p>{ trackName }</p>
-        <audio data-testid="audio-component" src={ trackViewUrl } controls>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
           O seu navegador não suporta o elemento
           { ' ' }
           <code>audio</code>
           .
         </audio>
+        <label
+          htmlFor={ trackId }
+          data-testid={ `checkbox-music-${trackId}` }
+        >
+          <input
+            type="checkbox"
+            name="favorited"
+            onChange={ onFavoriteChange }
+            checked={ favorited }
+            id={ trackId }
+          />
+          Favorita
+        </label>
       </section>
     );
   }
@@ -22,7 +54,10 @@ class MusicCard extends Component {
 
 MusicCard.propTypes = {
   trackName: propTypes.string.isRequired,
-  trackViewUrl: propTypes.string.isRequired,
+  previewUrl: propTypes.string.isRequired,
+  trackId: propTypes.number.isRequired,
+  favorited: propTypes.bool.isRequired,
+  onFavoriteChange: propTypes.func.isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,12 +3,22 @@ import propTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
 
 class Album extends Component {
-  state = {
-    result: null,
-    artistName: '',
-    collectionName: '',
+  constructor() {
+    super();
+
+    this.onFavoriteChange = this.onFavoriteChange.bind(this);
+
+    this.state = {
+      loading: false,
+      result: null,
+      artistName: '',
+      collectionName: '',
+      favoritesList: [],
+    };
   }
 
   componentDidMount() {
@@ -17,40 +27,62 @@ class Album extends Component {
       const result = await getMusics(id);
       const { artistName, collectionName } = result[0];
 
-      console.log(result);
-
       this.setState({ result, artistName, collectionName });
     });
   }
 
+  onFavoriteChange({ target: { id } }) {
+    this.setState({ loading: true }, async () => {
+      const { result, favoritesList } = this.state;
+      const track = result.slice(1).find(({ trackId }) => trackId === Number(id));
+      console.log(track);
+      await addSong(track);
+
+      this.setState({
+        loading: false,
+        favoritesList: [...favoritesList, Number(id)],
+      });
+    });
+  }
+
   render() {
-    const { result, artistName, collectionName } = this.state;
+    const { result, artistName, collectionName, loading, favoritesList } = this.state;
 
     return (
       <section data-testid="page-album">
         <Header />
-        <main>
-          <div>
-            <h2 data-testid="album-name">{ collectionName }</h2>
-            <strong data-testid="artist-name">{ artistName }</strong>
-          </div>
-          <section>
-            { result && (
-              result.slice(1)
-                .map(({
-                  trackId,
-                  trackName,
-                  trackViewUrl,
-                }) => (
-                  <MusicCard
-                    key={ trackId }
-                    trackName={ trackName }
-                    trackViewUrl={ trackViewUrl }
-                  />
-                ))
-            ) }
-          </section>
-        </main>
+        { loading
+          ? <Loading />
+          : (
+            <main>
+              <div>
+                <h2 data-testid="album-name">{ collectionName }</h2>
+                <strong data-testid="artist-name">{ artistName }</strong>
+              </div>
+              <section>
+                { result && (
+                  result.slice(1)
+                    .map(({
+                      trackId,
+                      trackName,
+                      previewUrl,
+                    }) => (
+                      <MusicCard
+                        key={ trackId }
+                        trackName={ trackName }
+                        trackId={ trackId }
+                        previewUrl={ previewUrl }
+                        favorited={
+                          favoritesList
+                            .some((id) => trackId === id)
+                        }
+                        onFavoriteChange={ this.onFavoriteChange }
+                      />
+                    ))
+                ) }
+              </section>
+            </main>
+          )}
       </section>
     );
   }
