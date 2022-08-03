2e5ad8a9bf9004220a88861ea0f980481edc98c7
Renan Fernandes <renzinestuods@gmail.com>
2022-08-03 19:03:23 -0300

    REQUSITO-11: Cria o formulário de edição de perfil

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,12 +1,140 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
 import Header from '../components/Header';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
+  constructor() {
+    super();
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onClickButtonSave = this.onClickButtonSave.bind(this);
+
+    this.state = {
+      loading: false,
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+      buttonSaveIsDisabled: true,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const user = await getUser();
+      const { name, email, image, description } = user;
+
+      this.setState({
+        name,
+        email,
+        image,
+        description,
+        loading: false,
+        concluded: false,
+      });
+    });
+  }
+
+  onInputChange({ target: { name, value } }) {
+    this.setState({ [name]: value }, () => {
+      const { name: userName, email, image, description } = this.state;
+      const userInfo = [userName, email, image, description];
+      const regex = /^\w+[@]{1}[a-z]+\.com{1}$/g;
+
+      const buttonSaveIsDisabled = !userInfo
+        .every((item) => item) || !regex.test(email);
+
+      this.setState({
+        buttonSaveIsDisabled,
+      });
+    });
+  }
+
+  onClickButtonSave() {
+    this.setState({ loading: true }, async () => {
+      const { name, email, image, description } = this.state;
+      await updateUser({ name, email, image, description });
+
+      this.setState({
+        loading: false,
+        concluded: true,
+      });
+    });
+  }
+
   render() {
+    const {
+      loading,
+      buttonSaveIsDisabled,
+      name,
+      email,
+      image,
+      description,
+      concluded,
+    } = this.state;
     return (
       <section data-testid="page-profile-edit">
         <Header />
-        ProfileEdit
+        {
+          loading
+            ? <Loading />
+            : (
+              <form>
+                <div>
+                  <strong>Nome</strong>
+                  <input
+                    type="text"
+                    name="name"
+                    value={ name }
+                    onInput={ this.onInputChange }
+                    data-testid="edit-input-name"
+                  />
+                </div>
+                <div>
+                  <strong>E-mail</strong>
+                  <input
+                    type="email"
+                    name="email"
+                    value={ email }
+                    onInput={ this.onInputChange }
+                    data-testid="edit-input-email"
+                  />
+                </div>
+                <div>
+                  <strong>Descrição</strong>
+                  <textarea
+                    name="description"
+                    value={ description }
+                    onInput={ this.onInputChange }
+                    cols="30"
+                    rows="10"
+                    data-testid="edit-input-description"
+                  />
+                </div>
+                <div>
+                  <strong>Foto</strong>
+                  <input
+                    type="text"
+                    name="image"
+                    value={ image }
+                    onInput={ this.onInputChange }
+                    data-testid="edit-input-image"
+                  />
+                </div>
+                <button
+                  type="button"
+                  disabled={ buttonSaveIsDisabled }
+                  onClick={ this.onClickButtonSave }
+                  data-testid="edit-button-save"
+                >
+                  Editar perfil
+                </button>
+              </form>
+            )
+        }
+        { concluded && <Redirect to="/profile" /> }
       </section>
     );
   }
