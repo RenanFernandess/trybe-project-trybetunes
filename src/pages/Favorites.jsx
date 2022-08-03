01752c8fad5e81b0215ab6ce1094059b8131e415
Renan Fernandes <renzinestuods@gmail.com>
2022-08-03 14:57:23 -0300

    REQUISITO-12: Cria a lista de músicas favoritas

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,73 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends Component {
+  state = {
+    loading: false,
+    favoritesList: null,
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, async () => {
+      const favoritesList = await getFavoriteSongs();
+
+      this.setState({
+        favoritesList,
+        loading: false,
+      });
+    });
+  }
+
+  onFavoriteChange = ({ target: { id } }) => {
+    this.setState({ loading: true }, async () => {
+      const { favoritesList } = this.state;
+      const track = favoritesList.find(({ trackId }) => trackId === Number(id));
+      await removeSong(track);
+      const newFavoritesList = await getFavoriteSongs();
+
+      this.setState({
+        favoritesList: newFavoritesList,
+        loading: false,
+      });
+    });
+  }
+
   render() {
+    const { loading, favoritesList } = this.state;
     return (
       <section data-testid="page-favorites">
         <Header />
-        Favorites
+        { loading
+          ? <Loading />
+          : (
+            <main>
+              <section>
+                { favoritesList && (
+                  favoritesList
+                    .map(({
+                      trackId,
+                      trackName,
+                      previewUrl,
+                    }) => (
+                      <MusicCard
+                        key={ trackId }
+                        trackName={ trackName }
+                        trackId={ trackId }
+                        previewUrl={ previewUrl }
+                        favorited={
+                          favoritesList
+                            .some(({ trackId: id }) => trackId === id)
+                        }
+                        onFavoriteChange={ this.onFavoriteChange }
+                      />
+                    ))
+                ) }
+              </section>
+            </main>
+          )}
       </section>
     );
   }
